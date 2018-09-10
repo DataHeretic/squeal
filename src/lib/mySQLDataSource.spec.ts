3d4131d0db79484a39b2bd92985601eb2d3cf3cf
Michael Melo <michael.melo@gmail.com>
2018-09-10 00:23:32 -0400

    Refactored spec to use async/await instead of chaining then's

--- src/lib/mySQLDataSource.spec.ts
+++ src/lib/mySQLDataSource.spec.ts
@@ -7,34 +7,13 @@ describe('MySQL datasource', () => {
         fixture = new MySQLDataSource('squeal', 'localhost', 3306, 'root', 'password');
     });
 
-    it('should connect to a MySQL database', (done) => {
-        fixture.connect().then(
-            () => done(),
-            (error) => { 
-                fail('error should not have occurred: ' + error)
-                done();
-            }
-        );
+    it('should connect to a MySQL database', async() => {
+        await fixture.connect();
     });
 
-    it('should send a query command to the MySQL database', (done) => {
-        fixture.connect().then(
-            () => {
-                return fixture.send('select 1');
-            },
-            (error) => { 
-                fail('error should not have occurred: ' + error)
-                done();
-            }
-        ).then(
-            (row) => {
-                expect(row).toBeTruthy();
-                done();
-            },
-            (error) => { 
-                fail('error should not have occurred: ' + error)
-                done();
-            }
-        );
+    it('should send a query command to the MySQL database', async() => {
+        await fixture.connect();
+        const result = await fixture.send('select 1');
+        expect(result).toBeTruthy();
     });
 });
